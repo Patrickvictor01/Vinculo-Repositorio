966c7480bb19705ccfec3d7dea364c498dc03a33
Patrick Victor <patrickvictor01@hotmail.com>
2023-04-24 21:35:55 -0300

    Main2.py

--- Main2.py
+++ Main2.py
@@ -1,1 +1,15 @@
-print("Olá mundo!")
+#Calcular IMC com Numpy
+import numpy as np
+
+altura = [1.87, 1.87, 1.82, 1.91, 1.90, 1.85]
+peso = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+#Criando Array na lista
+np_altura = np.array(altura)
+np_peso = np.array(peso)
+
+#Cálculo IMC
+IMC = np_altura/np_peso **2
+print(IMC)
+
+
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello World")
+#Pandas Básico
+import pandas as pd
+dict = {"país": ["Brasil", "Russia", "Índia", "China", "Africa do Sul"], "capital": ["Brasília", "Moscou", "New Dehli", "Pequim", "Pretóeia"],
+        "área": [8.516, 17.10, 3.286, 9.597, 1.221],
+        "população": [200.4, 143.5, 1252, 1357, 52.98]}
+brics = pd.DataFrame(dict)
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+print(brics)
